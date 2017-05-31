e2bc32ff3e39a40ebd4fc096fa3102ec354f62a8
bivarsoy <MhZ-cTz-E5f-hab>
2017-05-31 09:42:16 +0200

    Initial v1.0

---
+++ GitTrainingSolution/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConsoleApp1 v1.0");
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadLine();
+        }
+    }
+}
